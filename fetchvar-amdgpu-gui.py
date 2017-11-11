3487ad7b28f81c3f01fb89aa7af1e16735e7f575
AllAboutThatTEC <allaboutthattec@gmail.com>
2017-11-10 22:43:05 -0800

    v0.0.2

--- fetchvar-amdgpu-gui.py
+++ fetchvar-amdgpu-gui.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
+
 #part of AMDGPU-GUI by Thomas Collins
-#version 0.0.1
+#version 0.0.2
 #this isnt done yet its a work in progress
 #make sure you have lm-sensors installed
 #this only works on linux with amdgpu driver 
@@ -8,6 +10,7 @@
 
 import os
 import subprocess
+import re
 
 gpu_pre = subprocess.check_output(["sensors | grep amdgpu"], shell=True) #finds the name of the amd gpu im the system
 
@@ -23,8 +26,16 @@ data_pre = subprocess.check_output([fetch_temp_cmd], shell=True) #exectutes the 
 
 data = (data_pre.decode('ascii').strip()) #converts the output from bytes to ascii and removes skip line characters
 
-print (data) #prints the full decoded hopefully correct temp data
+data_file = open("temp_data.txt", "w") #create and open a file to store the temp data in
 
+data_file.write(data) #writes the gathered data to the text file
 
+data_file.close() #closes the file 
+
+#print (data) #prints the full decoded hopefully correct temp data
+
+temp1_input = subprocess.check_output(["cat temp_data.txt | grep temp1_input"], shell=True) #fetches the output of temp1_input 
+
+print (temp1_input.decode('ascii').strip())
 
 
